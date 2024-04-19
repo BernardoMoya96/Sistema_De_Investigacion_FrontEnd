d0af30358a341b6283e7fe8a36d326656a1750c2
Bernardo Moya <bennym@live.com.mx>
2024-04-19 00:16:21 -0600

    Changing port

--- src/app/app-config-props.ts
+++ src/app/app-config-props.ts
@@ -1,6 +1,6 @@
 export class AppConfig {
     public static HTTP_HOST : string = "localhost";
-    public static HTTP_PORT :string = "8080";
+    public static HTTP_PORT :string = "9180";
     public static HTTP_CONTEXT :string  = "sirms";
 
     public static COOKIE_KEY : string = "appCurrentUser" ;
